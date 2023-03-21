b21da62b27e22f2c8fc5c5e963237aefb5f707e2
wellington <wrogeriowrs@gmail.com>
2023-03-21 10:22:19 -0300

    .

--- src/controllers/RendimentosController.js
+++ src/controllers/RendimentosController.js
@@ -4,7 +4,7 @@ export const GetAll = async () => {
   const query = ` SELECT  Id, InstituicaoId, Instituicao, DtRendimento, Ano, MesNome, SaldoAnt, Saldo, Valor
                   FROM    vRendimentos
                   WHERE   DtRend >= CAST(CONCAT(YEAR(DATEADD(YEAR, -3, GETDATE())), '-', MONTH(DATEADD(MONTH, -4, GETDATE())), '-', 1) AS DATE)
-                  ORDER   BY DtRend DESC`;
+                  ORDER   BY DtRend DESC, Valor DESC`;
   try {
     await pool.connect();
     const result = await pool.request().query(query);
