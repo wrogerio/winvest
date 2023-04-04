43b415ade498942bb638ce137ffad986cfed1b5a
wellington <wrogeriowrs@gmail.com>
2023-04-04 07:56:14 -0300

    .

--- src/controllers/DividendosController.js
+++ src/controllers/DividendosController.js
@@ -4,7 +4,7 @@ export const GetAll = async () => {
   const query = ` SELECT  Id, FundoId, Sigla, Fundo, Cnpj, DtDividendo, MesNome, Qtd, Valor, Total
                   FROM    vDividendos
                   WHERE   DtDiv >= CAST(CONCAT(YEAR(DATEADD(YEAR, -3, GETDATE())), '-', MONTH(DATEADD(MONTH, -4, GETDATE())), '-', 1) AS DATE)
-                  ORDER   BY Sigla ASC`;
+                  ORDER   BY Ano Desc, Mes Desc, Sigla ASC`;
   try {
     await pool.connect();
     const result = await pool.request().query(query);
